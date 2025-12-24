1f4047f76c8d69725714382867dd67dd4d250806
BalinValery <valera.balin00@mail.ru>
2025-12-24 20:51:31 +0500

    F

--- Tyuiu.BalinVV.Sprint6.Task0.V17.Lib/DataService.cs
+++ Tyuiu.BalinVV.Sprint6.Task0.V17.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.BalinVV.Sprint6.Task0.V17.Lib
 {
-    public class Class1 : ISprint6Task0V17
+    public class DataService : ISprint6Task0V17
     {
         public double Calculate(int x)
         {
