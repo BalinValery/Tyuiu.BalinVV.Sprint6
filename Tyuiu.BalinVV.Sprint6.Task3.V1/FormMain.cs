b4aaa302c3102b4b3ed67cba9ab0dcebb187e10f
BalinValery <valera.balin00@mail.ru>
2025-12-26 02:09:46 +0500

    Task3

---
+++ Tyuiu.BalinVV.Sprint6.Task3.V1/FormMain.cs
@@ -0,0 +1,72 @@
+using Tyuiu.BalinVV.Sprint6.Task3.V1.Lib;
+namespace Tyuiu.BalinVV.Sprint6.Task3.V1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            DataService ds = new DataService();
+            int[,] mtrx = new int[5, 5] { { 25, 20, 28, 2, 6},
+                                      { 19, 9, 18, -7, 24},
+                                      { 21, 16, 5, -7, -8},
+                                      { 30, -13, 1, -15, 2 },
+                                      { 31, 9, -3, -9, -18 } };
+        private void buttonDone_ClickBVV(object sender, EventArgs e)
+        {
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.GetUpperBound(1) + 1;
+
+            dataGridViewResultBVV.ColumnCount = columns;
+            dataGridViewResultBVV.RowCount = rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewResultBVV.Columns[i].Width = 25;
+            }
+            int[,] res = ds.Calculate(mtrx);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewResultBVV.Rows[i].Cells[j].Value = Convert.ToString(res[i, j]);
+                }
+            }
+        }
+
+        private void buttonHelp_ClickBVV(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск3 Выполнил Балин В.В. СМАРТб-25-1", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.GetUpperBound(1) + 1;
+
+            dataGridViewMatrixBVV.ColumnCount = columns;
+            dataGridViewMatrixBVV.RowCount = rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewMatrixBVV.Columns[i].Width = 25;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewMatrixBVV.Rows[i].Cells[j].Value = Convert.ToString(mtrx[i, j]);
+                }
+            }
+        }
+
+        private void groupBoxInputData_EnterBVV(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+  
---
+++ Tyuiu.BalinVV.Sprint6.Task3.V1/Program.cs
@@ -0,0 +1,18 @@
+using Tyuiu.BalinVV.Sprint6.Task3.V1.Lib;
+namespace Tyuiu.BalinVV.Sprint6.Task3.V1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FormMain());
+        }
+    }
+}
